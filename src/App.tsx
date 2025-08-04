648c2509daf5b4d6fcc828afee97da4c578ae935
Panooh-code <info@panooh.com>
2025-08-04 12:09:14 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/sonner"; // Corrigido para usar o Sonner como Toaster principal
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/hooks/useAuth";
 import { ThemeProvider } from "@/contexts/ThemeContext";
-import "@/i18n";
 import PrivateRoute from "@/components/PrivateRoute";
+
+// Importação das páginas existentes
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import WizardPage from "./pages/WizardPage";
 import AuthPage from "./pages/AuthPage";
 import MyCardsPage from "./pages/MyCardsPage";
-import PublicCardPage from "./pages/PublicCardPage";
-import PublicCardViewPage from "./pages/PublicCardViewPage";
-
-import MyCustomerCardPage from "./pages/MyCustomerCardPage";
-import MyCustomerCardsPage from "./pages/MyCustomerCardsPage";
 import CustomerManagementPage from "./pages/CustomerManagementPage";
+import MyCustomerCardsPage from "./pages/MyCustomerCardsPage";
+import MyCustomerCardPage from "./pages/MyCustomerCardPage";
+
+// As páginas que foram removidas/substituídas já não são importadas
+import PublicCardPage from "./pages/PublicCardPage";
+import CustomerSignupPage from "./pages/CustomerSignupPage";
 
 const queryClient = new QueryClient();
 
@@ -27,28 +28,26 @@ const App = () => (
       <ThemeProvider>
         <AuthProvider>
           <Toaster />
-          <Sonner />
           <BrowserRouter>
             <Routes>
+              {/* Rotas Principais */}
               <Route path="/" element={<Index />} />
               <Route path="/wizard" element={<WizardPage />} />
               <Route path="/auth" element={<AuthPage />} />
+
+              {/* Rotas Privadas para Lojistas */}
               <Route path="/my-cards" element={<PrivateRoute><MyCardsPage /></PrivateRoute>} />
               <Route path="/my-cards/:cardId/customers" element={<PrivateRoute><CustomerManagementPage /></PrivateRoute>} />
+              
+              {/* Rotas Privadas para Clientes */}
               <Route path="/my-customer-cards" element={<PrivateRoute><MyCustomerCardsPage /></PrivateRoute>} />
+              <Route path="/my-card/:cardCode" element={<PrivateRoute><MyCustomerCardPage /></PrivateRoute>} />
               
-              {/* Public Card Routes */}
+              {/* Rotas Públicas dos Cartões (Simplificadas) */}
               <Route path="/card/:publicCode" element={<PublicCardPage />} />
-              <Route path="/card/:publicCode/view" element={<PublicCardViewPage />} />
+              <Route path="/card/:publicCode/signup" element={<CustomerSignupPage />} />
               
-              {/* Private Customer Card Route */}
-              <Route path="/my-card/:cardCode" element={
-                <PrivateRoute>
-                  <MyCustomerCardPage />
-                </PrivateRoute>
-              } />
-              
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              {/* Rota "Catch-all" para páginas não encontradas */}
               <Route path="*" element={<NotFound />} />
             </Routes>
           </BrowserRouter>
