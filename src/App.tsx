9c16bbc8c30f4853d1523d63d97420d09852b730
Panooh-code <info@panooh.com>
2025-08-04 12:17:38 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 
-
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/hooks/useAuth";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import PrivateRoute from "@/components/PrivateRoute";
+
+// Apenas importamos as páginas que sabemos que existem
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import WizardPage from "./pages/WizardPage";
 import AuthPage from "./pages/AuthPage";
 import MyCardsPage from "./pages/MyCardsPage";
-import PublicCardPage from "./pages/PublicCardPage";
-import PublicCardViewPage from "./pages/PublicCardViewPage";
-import CustomerSignupPage from "./pages/CustomerSignupPage";
-import MyCustomerCardPage from "./pages/MyCustomerCardPage";
 import MyCustomerCardsPage from "./pages/MyCustomerCardsPage";
+import MyCustomerCardPage from "./pages/MyCustomerCardPage";
 import CustomerManagementPage from "./pages/CustomerManagementPage";
+import PublicCardPage from "./pages/PublicCardPage"; // Vamos usar este como o ficheiro principal
 
 const queryClient = new QueryClient();
 
@@ -28,29 +26,23 @@ const App = () => (
       <ThemeProvider>
         <AuthProvider>
           <Toaster />
-          <Sonner />
           <BrowserRouter>
             <Routes>
+              {/* Rotas Principais */}
               <Route path="/" element={<Index />} />
-              <Route path="/wizard" element={<WizardPage />} />
               <Route path="/auth" element={<AuthPage />} />
+              <Route path="/wizard" element={<PrivateRoute><WizardPage /></PrivateRoute>} />
+
+              {/* Rotas Privadas */}
               <Route path="/my-cards" element={<PrivateRoute><MyCardsPage /></PrivateRoute>} />
               <Route path="/my-cards/:cardId/customers" element={<PrivateRoute><CustomerManagementPage /></PrivateRoute>} />
               <Route path="/my-customer-cards" element={<PrivateRoute><MyCustomerCardsPage /></PrivateRoute>} />
+              <Route path="/my-card/:cardCode" element={<PrivateRoute><MyCustomerCardPage /></PrivateRoute>} />
               
-              {/* Public Card Routes */}
+              {/* Rota Pública ÚNICA para o cartão */}
               <Route path="/card/:publicCode" element={<PublicCardPage />} />
-              <Route path="/card/:publicCode/view" element={<PublicCardViewPage />} />
-              <Route path="/card/:publicCode/signup" element={<CustomerSignupPage />} />
               
-              {/* Private Customer Card Route */}
-              <Route path="/my-card/:cardCode" element={
-                <PrivateRoute>
-                  <MyCustomerCardPage />
-                </PrivateRoute>
-              } />
-              
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              {/* Rota "Catch-all" */}
               <Route path="*" element={<NotFound />} />
             </Routes>
           </BrowserRouter>
